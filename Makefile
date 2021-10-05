7b7949ddbe6bf3db643874e1ee91588619e17922
baldiozh <baldiozh@icloud.com>
2021-10-05 14:05:32 +0300

    all done

--- Makefile
+++ Makefile
@@ -1,28 +1,44 @@
-NAME	= push_swap
+NAME		= push_swap
+NAME_bonus	= checker
 
-SRC		= push_swap.c \
-		ft_atoi.c ft_putstr_fd.c ft_putnbr.c ft_isdigit.c \
-		ft_catching_errors.c prev_sort.c ft_list_moves.c ft_owerflow.c \
-		push.c swap.c rotate.c reverse_rotate.c \
-		ft_count_listsize.c print_list.c remove_last_elem.c utils.c \
-		ft_sorting_3.c ft_sorting_4_5.c ft_sorting_100.c ft_sorting_500.c 
+SRC			= push_swap.c \
+			ft_atoi.c ft_putstr_fd.c ft_putnbr.c ft_isdigit.c \
+			ft_catching_errors.c prev_sort.c ft_list_moves.c ft_owerflow.c \
+			push.c swap.c rotate.c reverse_rotate.c \
+			ft_count_listsize.c print_list.c remove_last_elem.c utils.c \
+			ft_sorting_3.c ft_sorting_4_5.c ft_sorting_100.c ft_sorting_500.c 
+SRC_bonus	= bonus/checker.c \
+			bonus/ft_atoi.c bonus/ft_putstr_fd.c bonus/ft_putnbr.c bonus/ft_isdigit.c \
+			bonus/ft_catching_errors.c bonus/ft_list_moves.c bonus/ft_owerflow.c \
+			bonus/push.c bonus/swap.c bonus/rotate.c bonus/reverse_rotate.c \
+			bonus/ft_count_listsize.c bonus/remove_last_elem.c \
+			bonus/gnl_utils.c bonus/get_next_line.c \
+			bonus/ft_strncmp.c bonus/utils.c bonus/ft_is_stack_sorted.c
 
-OBJ		=	${SRC:.c=.o}
-FLAGS	=	-Wall -Werror -Wextra 
-HEADER	=	push_swap.h
+OBJ			=	${SRC:.c=.o}
+OBJ_bonus	=	${SRC_bonus:.c=.o}
+
+FLAGS		=	-Wall -Werror -Wextra 
+
+HEADER		=	push_swap.h
+HDR_bonus	=	checker.h
 
 all:		$(NAME)
-
 
 $(NAME):	$(OBJ) $(HEADER)
 			gcc $(FLAGS) $(OBJ) -o $(NAME)
 			rm -rf ${OBJ}
 clean:
-			rm -rf ${OBJ}
+			rm -rf ${OBJ} ${OBJ_bonus} 
 
 fclean: 	clean
-			rm -rf ${NAME} 
+			rm -rf ${NAME} ${NAME_bonus}
 
 re: 		fclean all
+
+bonus:		$(OBJ_bonus) $(HDR_bonus)
+			gcc $(FLAGS) $(OBJ_bonus) -o $(NAME_bonus)
+			rm -rf ${OBJ_bonus}
+			
 
 .PHONY: 	all clean fclean re
---
+++ checker.h
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmckinle <gmckinle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/29 19:13:53 by gmckinle          #+#    #+#             */
+/*   Updated: 2021/09/29 19:41:25 by gmckinle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHECKER_H
+# define CHECKER_H
+
+# ifndef	BUFFER_SIZE
+#  define	BUFFER_SIZE	4026
+# endif
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <stdlib.h>
+
+typedef struct s_elem
+{
+	int				value;
+	int				order;
+	struct s_elem	*next;
+}	t_elem;
+
+/* gnl */
+int		get_next_line(int fd, char **line);
+
+/* libft */
+size_t	ft_strlen(const char *str);
+char	*ft_join(char const *s1, char const *s2);
+char	*ft_strchr(char *str, int ch);
+char	*ft_strdup(char *src);
+
+int		ft_strncmp(const char *str1, const char *str2, size_t n);
+void	ft_putstr_fd(char *s, int fd);
+int		ft_atoi(const char *str);
+int		ft_isdigit(int ch);
+
+void	ft_putnbr(int n);
+
+/* errors */
+int		ft_catching_errors(int argc, char **argv);
+int		ft_checking_if_doubles(char **argv);
+int		ft_checking_if_digits(char **argv);
+int		ft_checking_if_sorted(char **argv);
+int		ft_check_if_overflow(char **argv);
+int		ft_owerflow(const char *str);
+
+/* list utils */
+t_elem	*create(int value);
+t_elem	*add_elem_start(int value, t_elem *head);
+void	add_elem_end(int value, t_elem *head);
+void	remove_last_elem(t_elem **head);
+
+/* swap */
+void	swap(t_elem **head);
+void	sa(t_elem **datum_a);
+void	sb(t_elem **datum_b);
+void	ss(t_elem **datum_a, t_elem **datum_b);
+
+/* push */
+void	push(t_elem **src, t_elem **dest);
+void	pa(t_elem **datum_a, t_elem **datum_b);
+void	pb(t_elem **datum_a, t_elem **datum_b);
+
+/* rotate */
+void	rotate(t_elem **head);
+void	ra(t_elem **datum_a);
+void	rb(t_elem **datum_b);
+void	rr(t_elem **datum_a, t_elem **datum_b);
+
+/* reverse rotate */
+void	reverse_rotate(t_elem **head);
+void	rra(t_elem **datum_a);
+void	rrb(t_elem **datum_b);
+void	rrr(t_elem **datum_a, t_elem **datum_b);
+
+/* list utils */
+void	ft_is_stack_sorted(t_elem **datum_a, t_elem **datum_b);
+int		ft_count_listsize(t_elem *head);
+
+/* utils */
+int		compare(char *line);
+void	just_do_it(char **line, t_elem **datum_a, t_elem **datum_b);
+
+void	freeList(t_elem **head);
+
+#endif
